c845df862b4b4b256eed4258ad4ed7f1614c04b8
SaidM2310 <l21210403@tectijuana.edu.mx>
2025-11-14 15:00:53 -0800

    Create Iclima interface for climate data retrieval

---
+++ src/Iclima.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public interface Iclima
+    {
+        string ObtenerTemperatura();
+        string ObtenerEstado();
+    }
+
+}
