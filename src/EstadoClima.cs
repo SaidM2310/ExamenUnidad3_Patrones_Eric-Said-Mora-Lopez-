10da866d2d6dfcf8c6fff8bf9cb753c1cfc49d9c
SaidM2310 <l21210403@tectijuana.edu.mx>
2025-11-14 15:02:02 -0800

    Add EstadoClima class to manage weather state

---
+++ src/EstadoClima.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class EstadoClima
+    {
+        public string Nombre { get; }
+
+        public EstadoClima(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+    }
+}
